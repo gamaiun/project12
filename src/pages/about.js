a45c2fb4dc3e1465d0f04816dc5d5fa5417e9271
gamaiun <antonevichi@gmail.com>
2023-09-22 14:20:09 +0300

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+const About = () => {
+  return (
+    <Layout pageTitle="About">
+      <div>
+        <p>I'm so snd so and I can do the following things:</p>
+        <ul>
+          <li>I do this</li>
+          <li>I do this</li>
+          <li>I do this</li>
+          <li>I do this</li>
+          <li>I do this</li>
+        </ul>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;

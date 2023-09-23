456867d73c46c0962e368f65d6b8c656294d1782
gamaiun <antonevichi@gmail.com>
2023-09-23 09:57:29 +0300

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Pagezzzzzzz">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="a forex graph" src="../images/example.png" />
     </Layout>
   );
 };
